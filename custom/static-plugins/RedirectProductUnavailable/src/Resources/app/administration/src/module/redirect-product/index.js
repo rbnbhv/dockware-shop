eb3562be7dc0f71c2f104418a082120ef1d2d3e2
Ruben Allenstein <ru.allenstein@reply.de>
2022-10-12 17:53:35 +0200

    [update] component imported

--- custom/static-plugins/RedirectProductUnavailable/src/Resources/app/administration/src/module/redirect-product/index.js
+++ custom/static-plugins/RedirectProductUnavailable/src/Resources/app/administration/src/module/redirect-product/index.js
@@ -2,8 +2,9 @@ import deDE from './snippet/de-DE.json';
 import enGB from './snippet/en-GB.json';
 
 import './extension/sw-product-detail-seo';
+import './component/redirect-product-category';
 
-Shopware.Module.register('redirectProduct', {
+Shopware.Module.register('redirect-product', {
     type: 'plugin',
     name: 'redirectProduct',
     title: 'redirect-product.general.title',
